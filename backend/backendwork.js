de6694bebf4fd9cbaf1eafe0c413dfe1dc0b5ab1
saicharan0312 <saicharan0312@gmail.com>
2023-11-03 21:23:14 -0400

    added all of them

---
+++ backend/backendwork.js
@@ -0,0 +1,43 @@
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+    host : "127.0.0.1",
+    user : "root",
+    password : "MySQL",
+    database : "restaurents"
+}).promise()
+
+export async function getAllRestaurents() {
+    const result = await pool.query("select * from restaurant_list");
+    return result[0];
+}
+
+export async function editRestaurent(id) {
+    console.log("iam here");
+    const single = await pool.query(`
+    select * from restaurant_list where id = ${id}
+    `)
+    return single[0];
+}
+
+export async function deleteRestaurent(id) {
+    console.log("working on delete backend", id);
+    console.log("iam here");
+    const single = await pool.query(`
+    delete from restaurant_list where id = ${id}
+    `)
+    return single[0];
+}
+
+export async function creatRestaurent(name, address, phone, email) {
+    const inserted = await pool.query(`
+    insert into restaurant_list (name, address, contact_phone, contact_email) values (?, ?, ?, ?)
+    `, [name, address, phone, email]);
+    return inserted;
+}
+// const restaurents = await getAllRestaurents();
+// const singleRestaurent = await editRestaurent(1);
+// const createdResaturent = await creatRestaurent("ahmed india", "adilabad", "888792922", "info5@gmail.com");
+// console.log(restaurents);
+// console.log(singleRestaurent);
+// console.log(createdResaturent);
